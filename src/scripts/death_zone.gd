d56e0675e3fcf0f473b1331f1928c368899022d6
Damglador <vse.stopchanskyi@gmail.com>
2025-05-17 21:18:53 +0200

    Automatically reload scene

--- src/scripts/death_zone.gd
+++ src/scripts/death_zone.gd
@@ -4,18 +4,21 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pass
+
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if is_instance_valid(body):
-			print("Node is valid")
 			if body.is_in_group("Platform"):
 				body.queue_free()
 			if body.is_in_group("Player"):
 				player.get_node("CharacterBody2D/Camera2D").position_smoothing_enabled = true
+				await get_tree().create_timer(0.5).timeout
+				get_tree().reload_current_scene()
 				pass
